38e30cb0974c870d59722d8eba7dae58a1d792a8
Nakajima Shunsuke <nakajima.sh@cnctor.jp>
2025-01-15 19:25:41 +0900

    Refactor Nuxt configuration by removing custom Vuetify integration hook
    - Simplified the Nuxt configuration by eliminating the custom hook for Vuetify integration with Vite.
    - Retained the '@nuxt/eslint' module for improved code quality enforcement while maintaining existing devtools settings.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,12 +2,6 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint',(_options, nuxt) => {
-    nuxt.hooks.hook('vite:extendConfig', (config) => {
-      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-      // @ts-expect-error
-      config.plugins.push(vuetify({ autoImport: true }));
-    });
-  }],
+  modules: ['@nuxt/eslint'],
 });
 

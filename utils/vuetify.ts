d30120c39009eb7dd5aec09de090bddd58d00b1e
Nakajima Shunsuke <nakajima.sh@cnctor.jp>
2025-01-22 12:21:33 +0900

    Enhance Vuetify configuration by adding icon support
    - Introduced MDI icon set support in the Vuetify utility file.
    - Configured default icon set and aliases for improved icon management.
    - Updated the Vuetify export to ensure proper integration of icons.
    
    This commit improves the UI component experience by enabling the use of Material Design Icons in the Vuetify setup.

--- utils/vuetify.ts
+++ utils/vuetify.ts
@@ -1,12 +1,21 @@
 import '@mdi/font/css/materialdesignicons.css'
 import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
+import { aliases, mdi } from 'vuetify/iconsets/mdi'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 
-export default createVuetify({
+const vuetify: ReturnType<typeof createVuetify> = createVuetify({
   components,
   directives,
+  icons: {
+    defaultSet: 'mdi',
+    aliases,
+    sets: {
+      mdi,
+    },
+  },
+
   theme: {
     defaultTheme: 'light',
     themes: {
@@ -31,3 +40,5 @@ export default createVuetify({
     },
   },
 })
+
+export default vuetify
